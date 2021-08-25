b5631761dfca2bca6c3da04f6df817a0bd09e7cf
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-08-25 19:43:05 -0300

    fix: conserta lint requisito 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,22 +1,27 @@
 from collections import Counter
 from datetime import datetime
 
+
 class SimpleReport:
     @classmethod
     def generate(cls, products):
         companies = Counter(product["nome_da_empresa"] for product in products)
-        most_common_company = companies.most_common(1)[0][0]
+        company = companies.most_common(1)[0][0]
 
-        oldest_manufacturing_date = min(product["data_de_fabricacao"] for product in products)
+        oldest_manufacturing_date = min(
+            product["data_de_fabricacao"] for product in products
+        )
 
         today_date = datetime.now().date()
         closest_expiration_date = min(
             (product["data_de_validade"] for product in products),
-            key = lambda date: abs(datetime.strptime(date,'%Y-%m-%d').date() - today_date)
+            key=lambda date: abs(
+                datetime.strptime(date, '%Y-%m-%d').date() - today_date
+            )
         )
 
         return (
             f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
             f"Data de validade mais próxima: {closest_expiration_date}\n"
-            f"Empresa com maior quantidade de produtos estocados: {most_common_company}\n"
+            f"Empresa com maior quantidade de produtos estocados: {company}\n"
         )
